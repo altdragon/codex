75c25b0588ee837c97232a13721c401f222741cf
altdragon <harrison518@gmai.com>
2023-01-29 19:31:15 -0600

    https://codex-dn06.onrender.com change

--- client/script.js
+++ client/script.js
@@ -83,7 +83,7 @@ const handleSubmit = async (e) => {
   loader(messageDiv);
 
   //fetch data from server -> bot's reply
-  const response = await fetch('http://localhost:5000', { 
+  const response = await fetch('https://codex-dn06.onrender.com', { 
     method: 'POST',
     headers: {
       'Content-Type': 'application/json'
